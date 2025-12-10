afc3ceba07ea5eefed634bc5d1fc852a0fe9c7aa
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-10 09:02:17 +0100

    F1: funcion recursiva

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -52,5 +52,13 @@ public class Factorial {
 
 		return resultado; 
 	}
+	
+	private static long factorialRec(int n) {
+	    if (n == 0 || n == 1) {
+	      return 1;
+	    }
+
+	    return n * factorialRec(n - 1);
+	}
 
 }
