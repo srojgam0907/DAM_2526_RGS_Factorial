f8ce586f5a7361c50929f0a78e85d80b02f5bd60
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-10 08:37:55 +0100

    Primera version, pide numero y comprueba

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -1,10 +1,23 @@
 package pruebaControlVersiones;
 
+import java.util.Scanner;
+
 public class Factorial {
-
+	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kb= new Scanner(System.in);
+		int num=0;
+		
+		System.out.print("Introduce un número entero mayor o igual que 0: ");
+		num= kb.nextInt();
+		
+		if (num < 0) {
+            System.out.println("ERROR: el número debe ser mayor o igual que 0.");
+        } else {
+            System.out.println("Has introducido el número: " + num);
+        }
+		
 	}
 
 }
