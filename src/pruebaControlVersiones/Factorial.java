82099fc078573d8bbfd67c43d1d836a858203d7a
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-10 08:43:34 +0100

    Refactorizacion, metodo pedirNumero

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -6,11 +6,10 @@ public class Factorial {
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		Scanner kb= new Scanner(System.in);
+	    Scanner kb = new Scanner(System.in);
 		int num=0;
 		
-		System.out.print("Introduce un número entero mayor o igual que 0: ");
-		num= kb.nextInt();
+		num= pedirNumero(kb); //LLamada al metodo
 		
 		if (num < 0) {
             System.out.println("ERROR: el número debe ser mayor o igual que 0.");
@@ -18,6 +17,11 @@ public class Factorial {
             System.out.println("Has introducido el número: " + num);
         }
 		
+	}
+	
+	private static int pedirNumero(Scanner kb) {
+	    System.out.print("Introduce un número entero mayor o igual que 0: ");
+	    return kb.nextInt();
 	}
 
 }
